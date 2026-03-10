68239642470bf2be3e707b9f0f267d7f978d2c4f
Hung X. Le <lexhung@gmail.com>
2026-03-10 18:03:16 +0700

    release v1.11

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ set -eu
 # Configuration
 # ---------------------------------------------------------------------------
 
-VERSION="1.10"
+VERSION="1.11"
 CHEZMOI_REPO=""
 GITHUB_USER=""
 DRY_RUN=0
