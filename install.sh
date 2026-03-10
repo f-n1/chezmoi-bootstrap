80f889452d6797fee924e982d6789088c36503cf
Hung X. Le <lexhung@gmail.com>
2026-03-10 17:14:21 +0700

    release v1.09

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ set -eu
 # Configuration
 # ---------------------------------------------------------------------------
 
-VERSION="1.08"
+VERSION="1.09"
 CHEZMOI_REPO=""
 GITHUB_USER=""
 DRY_RUN=0
